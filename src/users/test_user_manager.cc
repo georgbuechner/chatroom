f2da62bef35676a9f65eb8ab0c9ff2771d6c40a5
fux <jan.dick@bdp.org>
2020-10-26 14:14:56 +0100

    Modifies test-cases to give more information

--- src/users/test_user_manager.cc
+++ src/users/test_user_manager.cc
@@ -2,9 +2,6 @@
  * @author georgbuechner
  */
 
-#define CATCH_CONFIG_MAIN
-
-#include <fstream>
 #include <catch2/catch.hpp>
 #include <nlohmann/json.hpp>
 
@@ -16,29 +13,40 @@ TEST_CASE ("User manager can add a user", "[add_user]") {
   std::string username = "jan";
   std::string password1 = "password1234";
   std::string password2 = "password1234"; 
+  nlohmann::json answer;
 
-  nlohmann::json answer = user_manager.AddUser(username, password1, password2);
-  REQUIRE(answer["success"] == true);
-  REQUIRE(user_manager.GetUserByUsername(username) != nullptr);
+  SECTION("Adding a user correctly works.") {
+    answer = user_manager.AddUser(username, password1, password2);
+    REQUIRE(answer["success"] == true);
+    //REQUIRE(user_manager.GetUserByUsername(username) != nullptr);
+  }
 
-  answer = user_manager.AddUser(username, password1, password2);
-  REQUIRE(answer["error"] == "Username already exists.");
+  SECTION("Can't add multiple users with same usernames.") {
+    answer = user_manager.AddUser(username, password1, password2);
+    REQUIRE(answer["error"] == "Username already exists.");
+  }
 
-  username = "alex";
-  password2 = "password123";
-  answer = user_manager.AddUser(username, password1, password2);
-  REQUIRE(answer["error"] == "Passwords don't match.");
-  REQUIRE(user_manager.GetUserByUsername(username) == nullptr);
+  SECTION("Passwords need to match.") {
+    username = "alex";
+    password2 = "password123";
+    answer = user_manager.AddUser(username, password1, password2);
+    REQUIRE(answer["error"] == "Passwords don't match.");
+    //REQUIRE(user_manager.GetUserByUsername(username) == nullptr);
+  }
 
-  password1 = "password";
-  password2 = "password";
-  answer = user_manager.AddUser(username, password1, password2);
-  REQUIRE(answer["error"] == "Strength insufficient.");
-  REQUIRE(user_manager.GetUserByUsername(username== nullptr);
+  SECTION("Passwords need to have sufficient strength.") {
+    password1 = "password";
+    password2 = "password";
+    answer = user_manager.AddUser(username, password1, password2);
+    REQUIRE(answer["error"] == "Strength insufficient.");
+    //REQUIRE(user_manager.GetUserByUsername(username== nullptr);
+  }
 
-  password1 = "passwordpasswordpassword";
-  password2 = "passwordpasswordpassword";
-  answer = user_manager.AddUser(username, password1, password2);
-  REQUIRE(answer["success"] == true);
-  REQUIRE(user_manager.GetUserByUsername(username) != nullptr);
+  SECTION("Adding second users works as expected.") {
+    password1 = "passwordpasswordpassword";
+    password2 = "passwordpasswordpassword";
+    answer = user_manager.AddUser(username, password1, password2);
+    REQUIRE(answer["success"] == true);
+    //REQUIRE(user_manager.GetUserByUsername(username) != nullptr);
+  }
 }
