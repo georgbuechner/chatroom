d6ee0278426ed815e4faa0763e3db66e6ceba512
fux <jan.dick@bdp.org>
2020-10-26 16:06:11 +0100

    Adds comments to server test-file

--- src/server/test_server.cc
+++ src/server/test_server.cc
@@ -28,21 +28,31 @@ TEST_CASE ("server frame can handle post and get requests", "[requests]" ) {
         cl.set_connection_timeout(2);
 
         SECTION("Get-request serving files works!") {
+          //Non-existing page should return status 404 not found.
           auto resp = cl.Get("/humbug");
           REQUIRE(resp->status == 404);
 
+          //Existing page should return status 200 and content should be set.
           resp = cl.Get("/");
           REQUIRE(resp->status == 200);
           REQUIRE(resp->body.length() > 0);
         }
+
         SECTION("Post-Requests to handle registration, works") {
-          auto resp = cl.Post("/api/registration", {}, "{\"username\":\"jan\", \"password1\":\"0408\", \"password2\":\"0408\"}", "application/x-www-form-urlencoded");
+
+          //Check for correkt response when sending registration-request.
+          auto resp = cl.Post("/api/registration", {}, "{\"username\":\"jan\", "
+              "\"password1\":\"0408\", \"password2\":\"0408\"}", 
+              "application/x-www-form-urlencoded");
           REQUIRE(resp->status == 200);
 
-          resp = cl.Post("/api/registration", {}, "{}", "application/x-www-form-urlencoded");
+          //Check for Response with missing json entries
+          resp = cl.Post("/api/registration", {}, "{}", 
+              "application/x-www-form-urlencoded");
           REQUIRE(resp->status == 401);
           REQUIRE(resp->body == "Wrong json format: missing entries!");
 
+          //Check for respsonse with corrupted json (different error-codes).
           resp = cl.Post("/api/registration", {}, "{password:xxx}", 
               "application/x-www-form-urlencoded");
           REQUIRE(resp->status == 401);
