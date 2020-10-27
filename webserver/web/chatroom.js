7680caef7b50072d6c734dac71e3346b27915b8a
fux <jan.dick@bdp.org>
2020-10-27 12:40:52 +0100

    Adds more styling to chatroom

--- webserver/web/chatroom.js
+++ webserver/web/chatroom.js
@@ -50,20 +50,13 @@ setInterval(function(){
   xhttp.onload = function(event){    
     if (xhttp.status!=401) {    
       var json_response = JSON.parse(this.responseText);    
-      var table = document.createElement("ul");
-      for (var i=0; i<json_response.length; i++) {
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td");
-        td1.appendChild(document.createTextNode(json_response[i][0].toUpperCase() + " - "));
-        tr.appendChild(td1);
-        var td2 = document.createElement("td");
-        td2.appendChild(document.createTextNode(json_response[i][1]));
-        tr.appendChild(td2);
-
-        table.appendChild(tr); 
-      }
       document.getElementById("chatroom").innerHTML = "";
-      document.getElementById("chatroom").appendChild(table);
+      for (var i=0; i<json_response.length; i++) {
+        var div = document.createElement("div");
+        div.setAttribute("class", "msg");
+        div.innerHTML = json_response[i][0].toUpperCase() + " - " + json_response[i][1];
+        document.getElementById("chatroom").appendChild(div);
+      }
     }    
   };       
 }, 2500);
